5b9f7b6b9b892325d29692075ada1acf68db72bb
hyuzni <hyuzni@gmail.com>
2021-02-10 14:13:09 +0900

    git test

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,9 +1,9 @@
-import React from "react"
-import axios from "axios"
-import Movie from "./Movie"
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-import "./App.css"
-import "./Base.css"
+import './App.css';
+import './Base.css';
 
 // let something = ""
 // something.propTypes = {
@@ -17,13 +17,12 @@ import "./Base.css"
 // fetch() 보단 axios
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   state = {
     isLoading: true,
     movies: [],
-    12356 : 
-  }
+  };
   getMovies = async () => {
     // async와 await 는 세트임. 혼자서 쓰일 수 없음. es6 문법 참고
     //const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
@@ -33,26 +32,26 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
     // fetch() 보단 axios : fetch()의 layer 같은? 아이. npm install axios
-    console.log(movies)
-    this.setState({ movies, isLoading: false }) // state.movies : axios.movies 의 함축 표현. es6 표현
-  }
+    console.log(movies);
+    this.setState({ movies, isLoading: false }); // state.movies : axios.movies 의 함축 표현. es6 표현
+  };
   componentDidMount() {
     // component가 mount 되자마자 실행
-    this.getMovies()
-    console.log("===> componentDidMount")
+    this.getMovies();
+    console.log('===> componentDidMount');
   }
   componentDidUpdate() {
     // arg : prevProps, prevState, snapshot
     //setstate()
-    console.log("===> componentDidUpdate")
+    console.log('===> componentDidUpdate');
   }
   componentWillUnmount() {
-    console.log("===> componentWillUnmount")
+    console.log('===> componentWillUnmount');
   }
   render() {
-    const { isLoading, movies } = this.state // es6 : this.state.isLoading
+    const { isLoading, movies } = this.state; // es6 : this.state.isLoading
     return (
       <section className="container">
         {isLoading ? (
@@ -61,7 +60,7 @@ class App extends React.Component {
           </div>
         ) : (
           <div className="movies">
-            {movies.map((movie) => {
+            {movies.map(movie => {
               // map 을 사용할 때 return은 필수. 에러는 안나지만 주의.
               return (
                 <Movie
@@ -71,13 +70,13 @@ class App extends React.Component {
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
                 />
-              )
+              );
             })}
           </div>
         )}
       </section>
-    )
+    );
   }
 }
 
-export default App
+export default App;
